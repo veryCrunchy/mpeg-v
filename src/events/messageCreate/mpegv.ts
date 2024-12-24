0a5dd1daae161ec45bf51c4288521987916bcbad
dargy <dragonismbot@protonmail.com>
2024-12-23 20:52:00 -0600

    feat: prompt when unsupported type + add time

--- src/events/messageCreate/mpegv.ts
+++ src/events/messageCreate/mpegv.ts
@@ -1,4 +1,5 @@
 import { event, generateVideo } from "../../utils";
 export default event("messageCreate", async ({ log, client }, message) => {
+  if (client.user && message.author.id === client.user?.id) return;
   generateVideo(message, log, client);
 });
