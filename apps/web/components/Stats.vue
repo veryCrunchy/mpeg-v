520a52749a45765100f45746e67208c801c23c45
veryCrunchy <me@verycrunchy.dev>
2025-01-01 22:23:41 +0100

    feat: better divider

--- apps/web/components/Stats.vue
+++ apps/web/components/Stats.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="border-primary border-2 bg-discordMessage p-6 rounded-lg shadow-lg flex justify-around">
+  <div
+    class="border-primary border-2 bg-discordMessage p-6 rounded-lg shadow-lg grid grid-cols-3 divide-x divide-primary"
+  >
     <div class="text-center">
       <p class="text-3xl font-bold text-primary">192,202</p>
       <p class="text-neutral-100">Files Converted</p>
     </div>
-    <div class="border-l border-neutral-100 mx-4"></div>
     <div class="text-center">
       <p class="text-3xl font-bold text-primary">6,392</p>
       <p class="text-neutral-100">Servers Joined</p>
     </div>
-    <div class="border-l border-neutral-100 mx-4"></div>
     <div class="text-center">
       <p class="text-3xl font-bold text-primary">1,202</p>
       <p class="text-neutral-100">Users Served</p>
