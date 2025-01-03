82b816b46c8a7f35c73195c80ea55138bfc41ea3
veryCrunchy <me@verycrunchy.dev>
2025-01-04 00:03:51 +0100

    fix: web dockerfile

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,14 +1,5 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@nuxt/fonts"],
-
-  // fonts: {
-  //   google: {
-  //     families: {
-  //       Montserrat: [400, 600, 700, 800],
-  //     },
-  //   },
-  // },
-
   compatibilityDate: "2025-01-01",
 });
