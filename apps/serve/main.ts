8764a09f360cabedbf3029195e229eb5a98a60bc
dargy <hello@dargy.party>
2025-01-03 23:26:23 -0600

    chore: remove console.log

--- apps/serve/main.ts
+++ apps/serve/main.ts
@@ -1,13 +1,12 @@
 import generate from "./generate.ts";
 import { Authorization } from "@mpeg-v/utils";
 
-Deno.serve({ port: 4934 }, async (req) => {
+Deno.serve({ port: 3000 }, async (req) => {
   const url = new URL(req.url);
 
   // POST /generate
   if (url.pathname === "/generate" && req.method === "POST") {
     if (req.headers.get("Authorization") !== Authorization) {
-      console.log("FUCK!! they dont have the right key");
       return new Response(null, { status: 401 });
     }
     return await generate(req);
