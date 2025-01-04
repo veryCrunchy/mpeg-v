5fe84c4a6532f68f7255f73d80a8e0c6a611c876
dargy <dragonismbot@protonmail.com>
2025-01-03 19:09:18 -0600

    feat: cleaner errors

--- apps/bot/utils/embed.ts
+++ apps/bot/utils/embed.ts
@@ -1,20 +1,39 @@
-import { Embed } from "seyfert";
-import { ColorResolvable } from "seyfert/lib/common/types/resolvables.js";
+import { CommandContext, Embed, MenuCommandContext } from 'seyfert';
+import { MessageFlags } from 'seyfert/lib/types/index.js';
+import { ColorResolvable } from 'seyfert/lib/common/types/resolvables.js';
+
 export const COLORS: { [key: string]: ColorResolvable } = {
-  success: "#00FF00",
-  error: "#FF0000",
-  info: "#0000FF",
-  custom: "#FFA500",
+	success: '#00FF00',
+	error: '#FF0000',
+	info: '#0000FF',
+	custom: '#FFA500',
 };
 
 export const embed = ({
-  message,
-  status = "info",
-  color,
+	message,
+	status = 'info',
+	color,
 }: {
-  message: string;
-  status?: "success" | "error" | "info" | "custom";
-  color?: `#${string}`;
+	message: string;
+	status?: 'success' | 'error' | 'info' | 'custom';
+	color?: `#${string}`;
 }): Embed => {
-  return new Embed().setColor(color || COLORS[status]).setDescription(message);
+	return new Embed().setColor(color || COLORS[status]).setDescription(message);
+};
+
+export const handleError = (
+	ctx: MenuCommandContext<any, never> | CommandContext,
+	error: unknown,
+) => {
+	return ctx.editOrReply({
+		embeds: [
+			embed({
+				message: error instanceof Error
+					? `Error: ${error.message}`
+					: `Error: ${error}`,
+				status: 'error',
+			}),
+		],
+		flags: MessageFlags.Ephemeral,
+	});
 };
