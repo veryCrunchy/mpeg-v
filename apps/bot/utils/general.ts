770f2ac28a644851a4274f0415f5c99155cbf2ec
veryCrunchy <me@verycrunchy.dev>
2025-01-04 05:50:50 +0100

    feat: `capitalizeFirstLetter` util

--- apps/bot/utils/general.ts
+++ apps/bot/utils/general.ts
@@ -1,1 +1,4 @@
 export const ALLOWED_EXTENSIONS = ["mp3", "aac", "wav", "flac", "m4a", "ogg"];
+export function capitalizeFirstLetter(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
