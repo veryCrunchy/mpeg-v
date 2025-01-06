4a3d07ad7bff1dde9f4d1c9c29048cc67107518b
veryCrunchy <me@verycrunchy.dev>
2025-01-06 03:25:27 +0100

    feat: ping command

---
+++ apps/bot/commands/ping.ts
@@ -0,0 +1,17 @@
+import { Declare, Command, type CommandContext } from "seyfert";
+import { MessageFlags } from "seyfert/lib/types/index.js";
+
+@Declare({
+  name: "ping",
+  description: "Show latency with Discord",
+})
+export default class PingCommand extends Command {
+  override async run(ctx: CommandContext) {
+    // Average latency between existing connections
+    const ping = ctx.client.latency;
+    await ctx.write({
+      content: `The latency is \`${ping}\`ms | ${Deno.env.get("HOSTNAME")}`,
+      flags: MessageFlags.Ephemeral,
+    });
+  }
+}
